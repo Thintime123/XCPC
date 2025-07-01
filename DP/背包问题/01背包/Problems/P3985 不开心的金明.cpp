fbc9d336cf7e107ebb13e40e9e8fc581864f164d
Thin_time <2023040219@buct.edu.cn>
2025-07-02 00:01:58 +0800

    docs: Update 01背包、完全背包、多重背包的总结、例题与题解

--- DP/背包问题/01背包/Problems/P3985 不开心的金明.cpp
+++ DP/背包问题/01背包/Problems/P3985 不开心的金明.cpp
@@ -32,15 +32,52 @@ const int inf = 1e9;
 signed main() {
     IOS;
 
-    int n, W;
+    int n;
+    ll W;
     cin >> n >> W;
-    vector<int> Weight(n + 1), P(n + 1);
 
-    fer(i, 1, n + 1) {
-    	cin >> Weight[i] >> P[i];
+    vector<ll> v(n), p(n);
+    ll b = inf;
+    fer(i, 0, n) {
+        cin >> v[i] >> p[i];
+        b = min(b, v[i]);
     }
 
-    
+    // 四组：d = v[i] - b ∈ {0,1,2,3}
+    vector<ll> groups[4];
+    fer(i, 0, n) {
+        int d = int(v[i] - b);
+        groups[d].push_back(p[i]);
+    }
+
+    // 对每组排序并做前缀和
+    vector<vector<ll>> S(4);
+    fer(d, 0, 4) {
+        auto &G = groups[d];
+        sort(G.rbegin(), G.rend());  // 降序
+        S[d].assign(G.size() + 1, 0);
+        fer(k, 1, G.size() + 1) {
+            S[d][k] = S[d][k - 1] + G[k - 1];
+        }
+    }
+
+    // 四重枚举
+    ll ans = 0;
+    ll c[4] = { b, b+1, b+2, b+3 };
+    fer(k0, 0, groups[0].size() + 1) {
+        fer(k1, 0, groups[1].size() + 1) {
+            fer(k2, 0, groups[2].size() + 1) {
+                fer(k3, 0, groups[3].size() + 1) {
+                    ll cost = k0*c[0] + k1*c[1] + k2*c[2] + k3*c[3];
+                    if(cost > W) continue;
+                    ll val = S[0][k0] + S[1][k1] + S[2][k2] + S[3][k3];
+                    ans = max(ans, val);
+                }
+            }
+        }
+    }
+
+    cout << ans << "\n";   
 
     return 0;
 }
